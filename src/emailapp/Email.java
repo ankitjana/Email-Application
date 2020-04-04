842cae1a16a62e63b44830f2f343b1bad91774bb
ankitjana <ankit92005@gmail.com>
2020-04-04 17:42:49 -0400

    day1 commit

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,90 @@
 package emailapp;
 
-public class Email {
+import java.util.Scanner;
 
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String Department;
+	private String alternateEmail;
+	private int mailBoxCapacity = 500;
+	private int defaultPasswordLength = 10;
+	private String email;
+	private String company = "company";
+	//Constructor to receive first name and last name
+	public Email(String firstName, String lastName){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+		
+		this.Department = setDepartment();
+		//System.out.println("Department: " + this.Department);
+		
+
+		
+		this.email = constructEmail();
+		//System.out.println("Email ID: " + this.email);
+		
+		this.password = setRandomPwd(defaultPasswordLength);
+		//System.out.println("Password: "+ this.password);
+		
+	}
+	
+	//Ask for department
+	private String setDepartment() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Department Code:\n 1.Sales\n 2.Dev\n 3.Accounting");
+		int deptCode = sc.nextInt();
+		if(deptCode==1) {return "sales";}
+		else if(deptCode==2) {return "dev";}
+		else if(deptCode==3) { return "acct";}
+		else {return "";}
+		
+	}
+	
+	//Generate a Random Password
+	
+	private String setRandomPwd(int length) {
+		String pwdSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&";
+		char[] pwd= new char[length];
+		for(int i=0;i<length;i++) {
+			int rand = (int)(Math.random()*pwdSet.length());
+			pwd[i]=pwdSet.charAt(rand);
+		}
+		return new String(pwd);
+		
+	}
+	
+	//Construct Email
+	
+	private String constructEmail() {
+		String newEmail = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + Department +  "." + company + ".com";
+		return newEmail;
+	}
+	//Set the mailbox capacity
+	void setMailboxCapacity(int capacity) {
+		this.mailBoxCapacity = capacity;
+	}
+	
+	
+	//Set the alternate email
+	void setAlternateMail(String altmail) {
+		this.alternateEmail = altmail;
+	}
+	//Change the password
+	void changePassword(String newpwd) {
+		this.password = newpwd;
+	}
+	
+	//Get Methods
+	int getMailboxCapacity() {return mailBoxCapacity;}
+	String getAlternateEmail() {return alternateEmail;}
+	String getPassword() {return password;}
+	
+	//Show Info
+	String showInfo() {
+		return "Name: " + firstName + lastName + "\n" + "Email: " + email + "\n" + "Mailbox Capacity: " + mailBoxCapacity + "mb\n" + "Alternate Email: " + alternateEmail + "\n";
+	}
+	
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -1,5 +1,52 @@
 package emailapp;
 
-public class EmailApp {
+import java.util.Scanner;
 
+public class EmailApp {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter you name: ");
+		String fullname = sc.nextLine();
+		String[] splited = fullname.split("\\s+");
+		if(splited.length !=2) {
+			System.out.println("Wrong Format");
+			System.exit(0);
+		}
+		Email em1 = new Email(splited[0],splited[1]);
+		System.out.println(em1.showInfo());
+		int op = 0;
+		while(op!=6) {
+			System.out.println("Choose Options : \n" + "1. Change Email ID\n" + "2. Show Password\n" + "3. Change Password\n" + "4. Change Mailbox capacity\n" + "5. Show Information again\n" + "6. Exit");
+			op = Integer.parseInt(sc.nextLine());
+			if(op==1) {
+				System.out.println("Enter Alternate Email ID");
+				String newEmailID = sc.nextLine();
+				em1.setAlternateMail(newEmailID);
+			}
+			else if(op==2) {
+				System.out.println("Password: " + em1.getPassword());
+			}
+			else if(op==3) {
+				System.out.println("Enter New Password");
+				String newPwd = sc.nextLine();
+				em1.changePassword(newPwd);
+			}
+			else if(op==4) {
+				System.out.println("Enter New Mailbox Capacity");
+				int newCap = Integer.parseInt(sc.nextLine());
+				em1.setMailboxCapacity(newCap);
+			}
+			else if(op==5) {
+				System.out.println(em1.showInfo());
+			}
+			else if(op==6) {
+				System.exit(0);
+				
+			}
+			else {
+				System.out.println("Enter Correct Option");
+			}
+				
+			}
+	}
 }
